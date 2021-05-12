16c97f79ecb99becc2466b2adf2cfc66338837d2
kotsubo <hitoshi.kotsubo@fullon.co.jp>
2021-05-12 23:44:25 +0900

    future: TOPのタイトル変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel-react
                 </div>
 
                 <div class="links">
