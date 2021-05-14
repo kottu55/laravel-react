3818f6e5339e14cfa209cb689c6be293a2c5df36
kotsubo <hitoshi.kotsubo@fullon.co.jp>
2021-05-14 13:31:04 +0900

    future ログイン機能

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    ログイン中です！
                 </div>
             </div>
         </div>
