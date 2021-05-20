ae9e64c2acce67e633ba26b44e11d79fb33c77c8
kotsubo <hitoshi.kotsubo@fullon.co.jp>
2021-05-20 23:18:57 +0900

    rendering training card

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/layout.css') }}" rel="stylesheet">
 
     @yield('css')
 
@@ -27,6 +28,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
+                    <img style="width: 50px;" src="{{asset('images/logo.png')}}" alt="" class="navbar-logo">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -75,9 +77,12 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="main container">
             @yield('content')
         </main>
+        <footer class="footer p20">
+            <small class="copyright">Laravel-react 2021 copyright</small>
+        </footer>
     </div>
 </body>
 </html>
