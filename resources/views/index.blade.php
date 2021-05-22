8f6c3eab2392033e518d429114b7c7ae974f4970
kotsubo <hitoshi.kotsubo@fullon.co.jp>
2021-05-22 21:41:16 +0900

    トップ画面を作成。カード1枚を表示

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,9 +8,6 @@
             <div class="card-body text-center">
                 <div class='image-wrapper'><img class='book-image' style="width: 100%;" src="{{ asset('images/dummy.png') }}"></div>
                 <h3 class='h3 book-title'>ベンチプレス</h3>
-                <p class='description'>
-                    2021/10/11
-                </p>
                 <a href=""class='btn btn-secondary detail-btn'>記録する</a>
             </div>
         </div>
