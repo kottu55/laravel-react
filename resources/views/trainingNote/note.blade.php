ab1b0549849f8ca7a5753980c20d8698e320c378
kotsubo <hitoshi.kotsubo@fullon.co.jp>
2021-06-06 17:25:08 +0900

    カレンダー日付変更機能

--- resources/views/trainingNote/note.blade.php
+++ resources/views/trainingNote/note.blade.php
@@ -3,7 +3,11 @@
 @section('content')
 
     <h4>note page</h4>
-    <p>{{ $date }}</p>
+    <p>
+        <span><a href="{{ route('training-note', ['date' => $date->copy()->subDay()->format('Y-m-d') ]) }}"><<a></span>
+        {{ $date->format('Y-m-d') }}
+        <span><a href="{{ route('training-note', ['date' => $date->copy()->addDay()->format('Y-m-d') ]) }}">></a></span>
+    </p>
     @if (!$records->count())
         <p>今日の記録はありません。</p>
     @else
@@ -11,7 +15,8 @@
             <p>
                 <span>{{ $record->weight }}kg</span>
                 <span>{{ $record->reps }}rep</span>
-                <span><a href="{{ route('training-new')}}">{{ $selections->find($record->training_id)->name }}</a></span>
+                <span><a
+                        href="{{ route('training-new') }}">{{ $selections->find($record->training_id)->name }}</a></span>
             </p>
         @endforeach
     @endif
