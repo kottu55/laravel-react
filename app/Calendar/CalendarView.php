60aa43dc8b2505655c1dc2783f2e8ea293246df6
kotsubo <hitoshi.kotsubo@fullon.co.jp>
2021-07-16 00:13:28 +0900

    rendar calendar

--- app/Calendar/CalendarView.php
+++ app/Calendar/CalendarView.php
@@ -15,25 +15,42 @@ class CalendarView{
         return $this->carbon->format('Y年n月');
     }
 
-    function render(){
-        $html = [];
-        $html[] = '<div class="calendar">';
-        $html[] = '<table class="table">';
-        $html[] = '<thead>';
-        $html[] = '<tr>';
-        $html[] = '<th>月</th>';
-        $html[] = '<th>火</th>';
-        $html[] = '<th>水</th>';
-        $html[] = '<th>木</th>';
-        $html[] = '<th>金</th>';
-        $html[] = '<th>土</th>';
-        $html[] = '<th>日</th>';
-        $html[] = '</tr>';
-        $html[] = '</thead>';
-        $html[] = '</table>';
-        $html[] = '</div>';
-        return implode("", $html);
-    }
+	function render(){
+		$html = [];
+		$html[] = '<div class="calendar">';
+		$html[] = '<table class="table">';
+		$html[] = '<thead>';
+		$html[] = '<tr>';
+		$html[] = '<th>月</th>';
+		$html[] = '<th>火</th>';
+		$html[] = '<th>水</th>';
+		$html[] = '<th>木</th>';
+		$html[] = '<th>金</th>';
+		$html[] = '<th>土</th>';
+		$html[] = '<th>日</th>';
+		$html[] = '</tr>';
+		$html[] = '</thead>';
+
+		$html[] = '<tbody>';
+
+		$weeks = $this->getWeeks();
+		foreach($weeks as $week){
+		$html[] = '<tr class="'.$week->getClassName().'">';
+			$days = $week->getDays();
+			foreach($days as $day){
+				$html[] = '<td class="'.$day->getClassName().'">';
+				$html[] = $day->render();
+				$html[] = '</td>';
+			}
+			$html[] = '</tr>';
+		}
+
+		$html[] = '</tbody>';
+
+		$html[] = '</table>';
+		$html[] = '</div>';
+		return implode("", $html);
+	}
 
     protected function getWeeks(){
         $weeks = [];
